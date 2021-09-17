e143e74cac8c7cb4be5447826e113fe452f13911
santhoshixcube <santhoshisappa554@github.com>
2021-09-17 09:49:11 +0530

    updated

---
+++ Assets/TouchControl.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControl : MonoBehaviour
+{
+    [SerializeField]
+    private float speed,movement;
+    [SerializeField]
+    private Rigidbody2D Car;
+    public Rigidbody2D backTire, FrontTire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+      
+    }
+    /*private void FixedUpdate()
+    {
+            backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            FrontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            Car.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        
+
+    }*/
+    public void Movement()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase==TouchPhase.Began)
+            {
+                Touch touch1 = Input.GetTouch(0);
+                Car.AddForce(transform.right * speed);
+            }
+        } 
+    }
+    public void StopButton()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            speed = 0;
+            Car.AddForce(Vector2.zero);
+        }
+    }
+}
